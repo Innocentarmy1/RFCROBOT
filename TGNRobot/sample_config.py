79d18a0bba16138906b13bdc0586e47716cb7aed
ROCK ARMY <95131805+Innocentarmy1@users.noreply.github.com>
2022-02-03 02:24:33 +0530

    Update sample_config.py

--- TGNRobot/sample_config.py
+++ TGNRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/TGNRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/RFCROBOT/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "boom"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1669178360  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Timesisnotwaiting" 
-    SUPPORT_CHAT = "GodfatherSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1356458177  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "DEEWANA_MAHADEV_KA" 
+    SUPPORT_CHAT = "RFCTEAM"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001392103597
+        -
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001392103597
+        -1001788668145
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
